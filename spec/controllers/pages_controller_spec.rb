a0a1bb3143f59e8d2c6fc47a3d015e084b324664
jana <frau.sma@gmail.com>
2011-09-24 17:10:41 +0200

    Changed tests to contain a @base_title for the common part of the page titles.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = 'Sample App'
+  end
+
   # tests for Home page
 
   describe "GET 'home'" do
@@ -13,7 +17,7 @@ describe PagesController do
 
     it 'should have the right title' do
       get 'home'
-      response.should have_selector('title', :content => 'Sample App :: Home')
+      response.should have_selector('title', :content => @base_title + ' :: Home')
     end
   end
 
@@ -27,7 +31,7 @@ describe PagesController do
 
     it 'should have the right title' do
       get 'contact'
-      response.should have_selector('title', :content => 'Sample App :: Contact')
+      response.should have_selector('title', :content => @base_title + ' :: Contact')
     end
   end
 
@@ -41,7 +45,7 @@ describe PagesController do
 
     it 'should have the right title' do
       get 'about'
-      response.should have_selector('title', :content => 'Sample App :: About')
+      response.should have_selector('title', :content => @base_title + ' :: About')
     end
   end
 
