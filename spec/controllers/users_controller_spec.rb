ffc56af975d893995fab1fb488c6de009984ada3
jana <frau.sma@gmail.com>
2011-10-01 14:08:34 +0200

    Only signed-in users may now edit profiles (but can still edit each other's).

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -194,4 +194,24 @@ describe UsersController do
       end
     end
   end
+
+  describe 'authentication of edit/update pages' do
+
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    describe 'for non-signed-in users' do
+
+      it 'should deny access to the edit page' do
+        get :edit, :id => @user
+        response.should redirect_to(signin_path)
+      end
+
+      it 'should deny access to the update action' do
+        put :update, :id => @user, :user => {}
+        response.should redirect_to(signin_path)
+      end
+    end
+  end
 end
